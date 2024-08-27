2688ccb6d637cef86d3c8ea808e950cf5af567c4
jkompan <jkompan01@gmail.com>
2024-08-27 16:36:13 +0200

    point fcst models

--- load_mg_data.py
+++ load_mg_data.py
@@ -14,6 +14,8 @@ def load_microgrid_data(dir):
         if filename=='Building_1.csv':
             df = pd.read_csv(filepath)
             df['DayType'] = df['Day Type'].astype('int64').replace({2:'Mon',3:'Tue',4:'Wed',5:'Thu',6:'Fri',7:'Sat',8:'Holiday',1:'Sun'}).astype('category')
+            df['Month'] = df['Month'].astype('category')
+            df['Workday'] = df['DayType'].apply(lambda x: False if x in ['Sat','Sun','Holiday'] else True)
             df['DaylightSavings'] = df['Daylight Savings Status'].astype('int64').astype('category')
             df['Load'] = df.iloc[:,7]+df.iloc[:,8]+df.iloc[:,9]
             df[f'Load_{n}'] = df.iloc[:,7]+df.iloc[:,8]+df.iloc[:,9]
@@ -37,6 +39,9 @@ def load_microgrid_data(dir):
     for val in solar:
         df['SolarGen'] += val*temp.iloc[:,1]/100
 
+    # net load
+    df['NetLoad'] = df['Load'] - df['SolarGen']
+
     # rates
     # filepath = os.path.join(dir, 'prices.csv')
     # temp = pd.read_csv(filepath)
@@ -52,4 +57,17 @@ def load_microgrid_data(dir):
     filepath = os.path.join(dir, 'weather_data.csv')
     temp = pd.read_csv(filepath)
     df = pd.concat([df,temp],axis=1)
-    return df
+
+    df['Radiation [W/m2]'] = df['Diffuse Solar Radiation [W/m2]'] + df['Direct Solar Radiation [W/m2]']
+
+    # dummy timestamp for easier reference
+    df['Timestamp'] = pd.date_range(start='2016-01-01 01:00', periods=4*365*24, freq='h')
+
+    # move predictions for time t to observation t
+    for h in [6,12,24]:    
+        for id in [index for index, col in enumerate(df.columns) if str(h)+'h Prediction' in col]:
+            df.iloc[h:df.shape[0],id] = df.iloc[0:df.shape[0]-h,id]
+            
+    # drop first 24 hours due to lack of forecasts
+    df = df.iloc[24:]
+    df = df.reset_index(drop=True)
